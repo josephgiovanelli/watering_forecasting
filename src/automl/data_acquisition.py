05ff1360fbfd8a3b99b47216353804a0e468e267
Joseph Giovanelli <j.giovanelli@unibo.it>
2022-07-01 17:03:24 +0200

    Add agro data

--- src/automl/data_acquisition.py
+++ src/automl/data_acquisition.py
@@ -1,5 +1,7 @@
 # OpenML provides several benchmark datasets
 import openml
+import os
+
 import pandas as pd
 import numpy as np
 
@@ -17,3 +19,49 @@ def load_dataset_from_openml(id, encode=False):
         Xt[cat_features] = Xt[cat_features].replace("-1", np.nan)
         X = Xt.to_numpy()
     return X, y, categorical_indicator
+
+
+def load_agro_from_csv(path=os.path.join("/", "home", "data")):
+    def _ground_potential_reconstruction(df):
+        dfs = {}
+        for minute in [0, 15, 30, 45]:
+            dfs[minute] = df[
+                df.apply(
+                    lambda x: pd.to_datetime(x["timestamp"], unit="s").minute == minute,
+                    axis=1,
+                )
+            ]
+            if minute != 0:
+                dfs[minute]["timestamp"] = (
+                    (dfs[minute]["timestamp"] + 3600) / 3600
+                ).astype(int) * 3600
+            dfs[minute] = dfs[minute].pivot(
+                index="timestamp", columns=["z", "y", "x"], values="value"
+            )
+        for minute in [45, 30, 15]:
+            dfs[0] = dfs[0].combine_first(dfs[minute])
+        return dfs[0]
+
+    data = {}
+    for variable in [
+        "air_humidity",
+        "air_temperature",
+        "irrigation",
+        "precipitation",
+        "solar_radiation",
+        "wind_speed",
+        "ground_potential",
+    ]:
+        data[variable] = pd.read_csv(os.path.join(path, f"{variable}.csv"))
+        if variable == "ground_potential":
+            data[variable] = _ground_potential_reconstruction(data[variable].copy())
+            data[variable].columns = [
+                f"z{-column[0]}_y{column[1]}_x{column[2]}"
+                for column in data[variable].columns.values
+            ]
+        else:
+            data[variable] = data[variable].rename(columns={"value": variable})
+            data[variable] = data[variable].set_index("timestamp")
+
+    result = pd.concat(list(data.values()), axis=1)
+    return result.fillna(method="ffill")
