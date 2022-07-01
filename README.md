2d1b1258687732cec777da22091e51028e0fbd37
Joseph Giovanelli <j.giovanelli@unibo.it>
2022-07-01 18:01:37 +0200

    Add doc

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# watering_forecasting
- 
+# Structure
+- ```.devcontainer```: configuration to instantiate a dev container w/ vscode
+- ```data```: raw agro data for the analysis
+- ```resources```: example of the automl input (i.e., space) and output (i.e., visited confs)
+- ```scripts```: starting point for reproducibility
+- ```src```: source code
+
+# Reproducibility
+
+A Docker file is present to build the needed container.
+There are two options:
+- [JUST RUN] run either ```scripts/start.sh``` (unix) or ```scripts/start.bat``` (windows) to build the container and launch ```scripts/run_experiments.sh```, which already contains a configuration;
+- [RUN & DEBUG] open vscode, which should suggest you to open the project in the devcontainer.
+
+# Notes
+
+## Data acquisition
+
+In ```src/automl/data_acquisition.py```, there is a function that loads the raw data and provides a dataframe.
+
+## Rolling window
+
--- src/automl/data_acquisition.py
+++ src/automl/data_acquisition.py
@@ -6,12 +6,22 @@ import pandas as pd
 import numpy as np
 
 
-def load_dataset_from_openml(id, encode=False):
+def load_dataset_from_openml(id, decode=False):
+    """Load a dataset from openml
+
+    Args:
+        id (_type_): we can specify either the id or the name
+        decode (bool, optional): openml provides categorical data already encoded (w/ an OrdinalEncoder).
+        To simulate a real-case scenario (and have data decoded) turn this flag to True. Defaults to False.
+
+    Returns:
+        _type_: X is the data matrix, y is the data label, categorical_indicator is an array w/ a boolean for each feature (True if categorical, False otherwise)
+    """
     dataset = openml.datasets.get_dataset(id)
     X, y, categorical_indicator, _ = dataset.get_data(
         dataset_format="array", target=dataset.default_target_attribute
     )
-    if encode:
+    if decode:
         cat_features = [i for i, x in enumerate(categorical_indicator) if x == True]
         Xt = pd.DataFrame(X)
         Xt[cat_features] = Xt[cat_features].fillna(-1)
@@ -23,6 +33,19 @@ def load_dataset_from_openml(id, encode=False):
 
 def load_agro_data_from_csv(path=os.path.join("/", "home", "data")):
     def _ground_potential_reconstruction(df):
+        """Ground potential data likely contains missing values.
+        Since this kind of data has to be treated properly, we define a custom imputation.
+        Data from sensors are sampled every 15 minutes, but we are interested in hourly sampling.
+        We are lucky if we have the data at the :00 of the hour at hand , bc we do not have to do nothing.
+        Otherwise, we take the value of the previous quarter (15 minutes before).
+        This is done until the minute :00 of the previous hour.
+
+        Args:
+            df (_type_): input dataframe
+
+        Returns:
+            _type_: output dataframe
+        """
         dfs = {}
         for minute in [0, 15, 30, 45]:
             dfs[minute] = df[
