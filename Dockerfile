db8f510717e5ceb8680d9a67bebd0d4af0812890
Marco Meluzzi <marco.meluzzi@studio.unibo.it>
2022-10-12 15:06:50 +0200

    .

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
 FROM mcr.microsoft.com/vscode/devcontainers/python:0-3.9
+ARG DOCKER_USER=mmeluzzi
+RUN addgroup -S $DOCKER_USER && adduser -S $DOCKER_USER -G $DOCKER_USER
+USER $DOCKER_USER
 RUN apt-get update && apt-get install -y git --no-install-recommends
 COPY requirements.txt requirements.txt
 RUN pip install --upgrade pip && \
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-export UID=$(id -u)
-export GID=$(id -g)
 docker stop watering_forecasting
 docker rm watering_forecasting
 docker build -t watering_forecasting .
-docker run --user $UID:$GID --name watering_forecasting --volume $(pwd):/home/watering_forecasting --detach -t watering_forecasting
+docker run --name watering_forecasting --volume $(pwd):/home/watering_forecasting --detach -t watering_forecasting
 docker exec watering_forecasting python src/run_experiments.py
