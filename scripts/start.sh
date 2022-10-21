7b3ab2c5f8b7b5b70791baa131982e6e16874810
Marco Meluzzi <marco.meluzzi@studio.unibo.it>
2022-10-21 14:23:40 +0200

    Fix file ownership problem in Docker

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,5 +2,5 @@
 docker stop watering_forecasting
 docker rm watering_forecasting
 docker build -t watering_forecasting .
-docker run --name watering_forecasting --volume $(pwd):/home/watering_forecasting --detach -t watering_forecasting
+docker run -u $(id -u):$(id -g) --name watering_forecasting --volume $(pwd):/home/watering_forecasting --detach -t watering_forecasting
 docker exec watering_forecasting python src/run_experiments.py
