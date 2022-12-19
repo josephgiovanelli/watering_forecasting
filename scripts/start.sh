7844362fe138dea6b0d963bdcdb632d48e1dde6a
Marco Meluzzi <marco.meluzzi@studio.unibo.it>
2022-12-19 13:52:46 +0100

    Add tag management

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 docker stop watering_forecasting
 docker rm watering_forecasting
-docker build -t watering_forecasting .
-docker run -u $(id -u):$(id -g) --name watering_forecasting --volume $(pwd):/home/watering_forecasting --detach -t watering_forecasting
+docker run -u $(id -u):$(id -g) --name watering_forecasting --volume $(pwd):/home/watering_forecasting --detach -t ghcr.io/josephgiovanelli/watering_forecasting:0.1.0
 docker exec watering_forecasting /bin/bash -c "export PYTHONHASHSEED=42 && python src/run_experiments.py"
